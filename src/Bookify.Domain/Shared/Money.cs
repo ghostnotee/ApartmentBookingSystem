fb5914894a096f20e2d89102093b55a9db0f0d83
ghostNoté <selcukbilgen@hotmail.com>
2024-07-10 09:23:49 +0300

    typo fix

--- src/Bookify.Domain/Shared/Money.cs
+++ src/Bookify.Domain/Shared/Money.cs
@@ -4,7 +4,7 @@ public record Money(decimal Amount, Currency Currency)
 {
     public static Money operator +(Money first, Money second)
     {
-        if (first.Currency != second.Currency) throw new InvalidOperationException("Cuurencies have to be equal");
+        if (first.Currency != second.Currency) throw new InvalidOperationException("Currencies have to be equal");
 
         return first with { Amount = first.Amount + second.Amount };
     }
