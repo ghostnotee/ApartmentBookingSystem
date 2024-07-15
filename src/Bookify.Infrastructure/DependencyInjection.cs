922096940a13a1e841387629eca09dafe390d3e1
ghostNoté <selcukbilgen@hotmail.com>
2024-07-15 03:14:23 +0300

    Implementing the SQL connection factory

--- src/Bookify.Infrastructure/DependencyInjection.cs
+++ src/Bookify.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,14 @@
 using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Data;
 using Bookify.Application.Abstractions.Email;
 using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings;
 using Bookify.Domain.Users;
 using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Repositories;
+using Dapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,14 +23,14 @@ public static class DependencyInjection
         services.AddTransient<IEmailService, IEmailService>();
 
         var connectionString = configuration.GetConnectionString("Database") ?? throw new ArgumentNullException(nameof(configuration));
-        services.AddDbContext<ApplicationDbContext>(optionsBuilder =>
-        {
-            optionsBuilder.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
-        });
+        services.AddDbContext<ApplicationDbContext>(optionsBuilder => { optionsBuilder.UseNpgsql(connectionString).UseSnakeCaseNamingConvention(); });
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IApartmentRepository, ApartmentRepository>();
         services.AddScoped<IBookingRepository, BookingRepository>();
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+        services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
+        SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
+
         return services;
     }
 }
