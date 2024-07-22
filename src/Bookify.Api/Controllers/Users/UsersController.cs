42f81474563a18e538e33e0c179937acdf26bd37
ghostNoté <selcukbilgen@hotmail.com>
2024-07-23 00:05:29 +0300

    Adding an API endpoint for registering a user.

---
+++ src/Bookify.Api/Controllers/Users/UsersController.cs
@@ -0,0 +1,21 @@
+using Bookify.Application.Users.RegisterUser;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Users;
+
+[ApiController]
+[Route("api/users")]
+public class UsersController(ISender sender) : ControllerBase
+{
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterUserRequest request, CancellationToken cancellationToken)
+    {
+        var command = new RegisterUserCommand(request.Email, request.FirstName, request.LastName, request.Password);
+        var result = await sender.Send(command, cancellationToken);
+        if (result.IsFailure) return BadRequest(result.Error);
+        return Ok(result.Value);
+    }
+}
