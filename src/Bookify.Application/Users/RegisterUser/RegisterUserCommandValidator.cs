58888d1a9e599b6f0b272231968e0ae09f5a0476
ghostNoté <selcukbilgen@hotmail.com>
2024-07-22 23:50:01 +0300

    Implementing the user registration feature

---
+++ src/Bookify.Application/Users/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+
+namespace Bookify.Application.Users.RegisterUser;
+
+public class RegisterUserCommandValidator:AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty();
+        RuleFor(x => x.LastName).NotEmpty();
+        RuleFor(x => x.Email).EmailAddress();
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(5);
+    }
+}
