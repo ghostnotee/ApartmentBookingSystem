e9e51740e04efb5d51268345fbb38f61c0332431
ghostNoté <selcuk.bilgen@hotmail.com>
2024-08-26 00:03:43 +0300

    Functional Testing.

--- src/Bookify.Infrastructure/Authentication/AuthenticationOptions.cs
+++ src/Bookify.Infrastructure/Authentication/AuthenticationOptions.cs
@@ -4,7 +4,7 @@ public sealed class AuthenticationOptions
 {
     public const string SectionName = "Authentication";
     public string Audience { get; init; } = string.Empty;
-    public string MetadataUrl { get; init; } = string.Empty;
+    public string MetadataUrl { get; set; } = string.Empty;
     public bool RequireHttpsMetadata { get; init; }
-    public string Issuer { get; init; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
 }
